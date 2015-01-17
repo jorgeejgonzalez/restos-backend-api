5e4489cc0d7425c5d2b3341f9f2d9a0793e1e2f4
Jorge Gonzalez <jorgeejgonzalez@gmail.com>
2015-01-17 12:02:34 -0430

    Se agregaron los metodos al AdministradorUsuarios

--- src/main/java/org/idisoft/restos/administracionusuarios/AdministradorUsuarios.java
+++ src/main/java/org/idisoft/restos/administracionusuarios/AdministradorUsuarios.java
@@ -1,5 +1,17 @@
 package org.idisoft.restos.administracionusuarios;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.NoResultException;
+import javax.validation.ValidationException;
+
+import org.apache.http.auth.AuthenticationException;
+
 public interface AdministradorUsuarios {
+	
+	public Usuario auntenticarUsuario(final String login, final String password) 
+	throws NoResultException,ValidationException,AuthenticationException;
+	
+	public Usuario registrarUsuario(final Usuario usuario)
+	throws EntityExistsException, ValidationException;
 
 }
